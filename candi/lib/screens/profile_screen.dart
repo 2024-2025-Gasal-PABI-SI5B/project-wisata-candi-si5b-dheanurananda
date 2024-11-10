663c79e996bc361bac87e8693b70272d1d3cf10d
dheanurananda <dheanur480@gmail.com>
2024-11-11 00:54:37 +0700

    membuat todo 1,padding

--- candi/lib/screens/profile_screen.dart
+++ candi/lib/screens/profile_screen.dart
@@ -6,6 +6,12 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
+  // TODO: 1 Deklarasikan variabel yang dibutuhkan
+  bool isSignedIn = false;
+  String fulName = '';
+  String userName = '';
+  int favoriteCandiCount = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,8 +22,16 @@ class _ProfileScreenState extends State<ProfileScreen> {
             width: double.infinity,
             color: Colors.deepPurple,
           ),
-          Column(
-            children: [],
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              children: [
+                //TODO: 2 Buat bagian Profile header (isinya gambar profile)
+
+                //TODO  3 Buat bagianprofile info (isinya info profile)
+                //TODO: 4 Buat bagian profile action (tombol signin/sigout)
+              ],
+            ),
           ),
         ],
       ),

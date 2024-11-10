8824ad1f7666abf00780e0bc0c30338f0cd87737
dheanurananda <dheanur480@gmail.com>
2024-11-11 01:27:13 +0700

    membuat todo 2

--- candi/lib/screens/profile_screen.dart
+++ candi/lib/screens/profile_screen.dart
@@ -7,7 +7,7 @@ class ProfileScreen extends StatefulWidget {
 
 class _ProfileScreenState extends State<ProfileScreen> {
   // TODO: 1 Deklarasikan variabel yang dibutuhkan
-  bool isSignedIn = false;
+  bool isSignedIn = true;
   String fulName = '';
   String userName = '';
   int favoriteCandiCount = 0;
@@ -27,7 +27,37 @@ class _ProfileScreenState extends State<ProfileScreen> {
             child: Column(
               children: [
                 //TODO: 2 Buat bagian Profile header (isinya gambar profile)
-
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 150),
+                    child: Stack(
+                      alignment: Alignment.bottomRight,
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                              border: Border.all(
+                                color: Colors.deepPurple,
+                                width: 2,
+                              ),
+                              shape: BoxShape.circle),
+                          child: CircleAvatar(
+                            radius: 50,
+                            backgroundImage:
+                                AssetImage('images/placeholder_image.png'),
+                          ),
+                        ),
+                        if (isSignedIn)
+                          IconButton(
+                              onPressed: () {},
+                              icon: Icon(
+                                Icons.camera_alt,
+                                color: Colors.deepPurple[50],
+                              ))
+                      ],
+                    ),
+                  ),
+                )
                 //TODO  3 Buat bagianprofile info (isinya info profile)
                 //TODO: 4 Buat bagian profile action (tombol signin/sigout)
               ],
